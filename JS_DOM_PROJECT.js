6c0ea5d7a98df26c8aa4cb6e6773539cd712a8d6
Aayush Rijal <rijalaayush77@gmail.com>
2026-06-19 21:26:24 +0545

    Started JS DOM and worked a few programs to understand the connection between HTML and JS

---
+++ JS_DOM_PROJECT.js
@@ -0,0 +1,37 @@
+// TASK 1 JS
+
+// const dummy_var = document.getElementById("title");
+
+// dummy_var.textContent;
+// dummy_var.textContent = "Welcome back, Aayush";
+// //Question? Can we not just initialize and assign at the same time?
+
+// const para = document.getElementsByTagName("p");
+
+// para.style.color = "steelblue";
+
+// para.classList.add("Active");
+
+
+//TASK 2 JS
+// const cards = document.querySelectorAll(".card");  
+
+// cards.forEach((card, index) => {
+//     card.classList.add("visible");
+//     card.textContent = `Card ${index + 1}`;  // note capital C in textContent
+// });
+
+//Struggled and had to use AI to understand what this problem was actually asking. I ddinot write this code myself but I do understnad what it does
+
+//TASK 3 JS
+// const products = document.querySelectorAll("li");
+
+// products.forEach((prod, index) => {
+//     if(Number(prod.dataset.price)>400){
+//         prod.classList.add("Expensive");
+//         prod.style.color = "crimson";
+//     }
+//     console.log(Number(prod.dataset.price)>400);
+// });
+
+use querySelector from now on. One selector to rule them all
